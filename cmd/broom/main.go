832ee5fe9b953c4d2fc0fb98a9ea12b25fa95cb4
Matous Kucera <mk@kcra.me>
2022-09-29 23:02:25 +0200

    [release] feat: check services for known-malicious service

--- cmd/broom/main.go
+++ cmd/broom/main.go
@@ -6,9 +6,30 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/sys/windows"
+	"golang.org/x/sys/windows/svc/mgr"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 )
+
+// SystemdSearchPaths are the base directories to search in for service files.
+var SystemdSearchPaths = []string{
+	// TODO: include user unit paths
+	"/etc/systemd/system.control/",
+	"/run/systemd/system.control/",
+	"/run/systemd/transient/",
+	"/run/systemd/generator.early/",
+	"/etc/systemd/system/",
+	"/etc/systemd/system.attached/",
+	"/run/systemd/system/",
+	"/run/systemd/system.attached/",
+	"/run/systemd/generator/",
+	"/usr/lib/systemd/system/",
+	"/run/systemd/generator.late/",
+}
 
 // main is the application entrypoint.
 func main() {
@@ -24,6 +45,7 @@ func main() {
 			},
 		},
 		Action: func(cCtx *cli.Context) error {
+			foundService := false
 			foundInfected := false
 			err := filepath.Walk(filepath.Clean(cCtx.String("dir")), func(path string, info os.FileInfo, err error) error {
 				if err != nil {
@@ -64,11 +86,56 @@ func main() {
 				return err
 			}
 
+			if runtime.GOOS == "windows" {
+				var s *uint16
+				h, err := windows.OpenSCManager(s, nil, windows.SC_MANAGER_ENUMERATE_SERVICE)
+				if err != nil {
+					color.Red("could not query windows services (missing access?)")
+				} else {
+					svcMgr := &mgr.Mgr{Handle: h}
+					services, err := svcMgr.ListServices()
+					if err != nil {
+						color.Red("could not query windows services")
+					} else {
+						for _, svc := range services {
+							if svc == "vmd-gnu" {
+								color.Red("found 'vmd-gnu' service")
+								foundService = true
+							}
+						}
+					}
+				}
+			} else {
+				for _, dir := range SystemdSearchPaths {
+					if info, err := os.Stat(dir); !os.IsNotExist(err) && info.IsDir() {
+						err := filepath.Walk(dir, func(path string, info0 fs.FileInfo, err error) error {
+							if err != nil {
+								return err
+							}
+							if !info0.IsDir() && strings.TrimSuffix(filepath.Base(path), filepath.Ext(path)) == "vmd-gnu" {
+								color.Red("found %s service", path)
+								foundService = true
+							}
+
+							return nil
+						})
+						if err != nil {
+							color.Red("could not query services at path %s", dir)
+						}
+					}
+				}
+			}
+
 			fmt.Print("\n\n\n\n") // a bit of space before the assessment
 			if foundInfected {
-				color.Red("JAR files containing files that are a known symptom of the malware were found.")
+				color.Red("JAR files containing files that are a known part of the malware were found.")
 			} else {
 				color.Green("No files related to the malware were found!")
+			}
+			if foundService {
+				color.Red("A system service that is a known part of the malware was found.")
+			} else {
+				color.Green("No services related to the malware were found!")
 			}
 
 			fmt.Print("Press Enter to exit...")
--- go.mod
+++ go.mod
@@ -10,5 +10,5 @@ require (
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.17.1 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
 )
